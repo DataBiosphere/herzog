47e5ced2a9e69920b3c451ecca2e23104edd6a8c
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2021-07-12 11:02:52 -0600

    Improve markdown support, ipynb formatting (#34)

--- herzog/parser.py
+++ herzog/parser.py
@@ -13,36 +13,56 @@ JUPYTER_SHELL_PFX = "#!"
 JUPYTER_MAGIC_PFX = "#%"
 
 class HerzogCell:
-    _translate = {CellType.python: "code", CellType.markdown: "markdown"}
-
-    def __init__(self, cell_type: CellType, lines: Iterable[str]):
-        self.cell_type = cell_type
-        self.lines: List[str] = list()
-        for line in lines:
-            if CellType.python == self.cell_type:
-                if "pass" == line:
-                    pass
-                elif line.startswith(JUPYTER_SHELL_PFX) or line.startswith(JUPYTER_MAGIC_PFX):
-                    self.lines.append(line[1:])
-                else:
-                    self.lines.append(line)
-            elif CellType.markdown == self.cell_type:
-                if line in ('"""', "pass"):
-                    pass
-                else:
-                    self.lines.append(line)
-
-    @property
-    def has_ipynb_representation(self) -> bool:
-        return self.cell_type in self._translate
+    cell_type: CellType
+    has_ipynb_representation: bool
 
     def to_ipynb_cell(self) -> Dict[str, Any]:
-        jupyter_cell = dict(cell_type=self._translate[self.cell_type],
-                            metadata=dict(),
-                            source=os.linesep.join(self.lines))
-        if CellType.python == self.cell_type:
-            jupyter_cell.update(dict(execution_count=None, outputs=list()))
-        return jupyter_cell
+        raise NotImplementedError()
+
+class PythonCell(HerzogCell):
+    cell_type = CellType.python
+    has_ipynb_representation = True
+
+    def __init__(self, indent: int, lines: Iterable[str]):
+        self.lines = list()
+        for line in lines:
+            line = line[indent:].rstrip() + os.linesep
+            if "pass" == line:
+                pass
+            elif line.startswith(JUPYTER_SHELL_PFX) or line.startswith(JUPYTER_MAGIC_PFX):
+                self.lines.append(line[1:])
+            else:
+                self.lines.append(line)
+
+    def to_ipynb_cell(self) -> Dict[str, Any]:
+        return dict(cell_type="code",
+                    metadata=dict(),
+                    source=self.lines,
+                    execution_count=None,
+                    outputs=list())
+
+class MarkdownCell(HerzogCell):
+    cell_type = CellType.markdown
+    has_ipynb_representation = True
+
+    def __init__(self, indent: int, text: str):
+        lines = text.split(os.linesep)
+        while lines:
+            if not lines[0].strip():
+                lines.pop(0)
+            else:
+                break
+        while lines:
+            if not lines[-1].strip():
+                lines.pop()
+            else:
+                break
+        self.lines = [line[indent:] + os.linesep for line in lines]
+
+    def to_ipynb_cell(self) -> Dict[str, Any]:
+        return dict(cell_type="markdown",
+                    metadata=dict(),
+                    source=self.lines)
 
 def parse_cells(raw_lines: TextIO) -> Generator[HerzogCell, None, None]:
     lines = [line for line in raw_lines]
@@ -53,6 +73,7 @@ def parse_cells(raw_lines: TextIO) -> Generator[HerzogCell, None, None]:
                 ast_call = node.items[0].context_expr
                 if "herzog" == ast_call.func.value.id and "Cell" == ast_call.func.attr:  # type: ignore
                     cell_type = CellType[ast_call.args[0].value]  # type: ignore
-                    cell_lines = [line[indent:].rstrip()
-                                  for line in lines[node.body[0].lineno - 1: node.body[-1].end_lineno]]
-                    yield HerzogCell(cell_type, cell_lines)
+                    if CellType.python == cell_type:
+                        yield PythonCell(indent, lines[node.body[0].lineno - 1: node.body[-1].end_lineno])
+                    elif CellType.markdown == cell_type:
+                        yield MarkdownCell(indent, node.body[0].value.value)  # type: ignore
